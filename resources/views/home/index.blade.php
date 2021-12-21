134f53284a9d3e27af32762dedb413252abb83d7
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-21 20:06:32 +0100

    prints the cards using the comic.php database

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,7 +8,39 @@
 
 @section('content')
     {{-- qui si trova il main --}}
-    <h1>contenuto principale</h1>
+    <main>
+        <div class="container">
+            
+            <div class="badge">
+                <h2>
+                    CURRENT SERIES
+                </h2>
+            </div>
+
+            @foreach ($comics as $item)
+                <div class="single_card_container">
+                    <div class="comic_thumb_container">
+                        <img src="{{$item['thumb']}}" alt="">
+                    </div>
+                    <div class="comic_title">
+                        @php
+
+                            /* if(str_contains($item["title"], "#")){
+                                $actualTitle = explode('#', $item['title'])
+                            }else if(str_contains($item["title"], "Vol"){
+                                $actualTitle = explode('Vol', $item['title'])
+                            } */
+                            $actualTitle = explode('#', $item['title'])
+
+                        @endphp
+                            
+                        <p>{{ strtoupper($actualTitle[0]) }}</p>
+                    </div>
+                </div>
+            @endforeach
+
+        </div>
+    </main>
 @endsection
 
 {{-- qui si trova il footer --}}
