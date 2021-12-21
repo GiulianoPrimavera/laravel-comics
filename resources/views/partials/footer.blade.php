3e30d8ba6a865e3047b80478e38e214750a11bb5
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-21 17:36:04 +0100

    adds top footer section

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -53,16 +53,32 @@
 @endphp
 
 <footer>
-    <div class="container">
-        @foreach ($footer_links as $link)
-            <h3 class="footer_link_title">
-                {{$link["link_title"]}}
-            </h3>
-            
-            @foreach ($link["links"] as $singleLink)
-                <a href="">{{$singleLink}}</a>
-            @endforeach
-        @endforeach
+    <div class="top_footer">
+        <div class="container">
+            <div class="row">
+                <div class="footer_links_container col">
+    
+                    @foreach ($footer_links as $link)
+                        <p class="footer_link_title">
+                            {{$link["link_title"]}}
+                        </p>
+                        
+                        @foreach ($link["links"] as $singleLink)
+                            <a href="">{{$singleLink}}</a>
+                        @endforeach
+                    @endforeach
+                    
+                </div>
+    
+                <div class="footer_logo_container col">
+                    <img src="{{ asset('img/dc-logo-bg.png') }}" alt="">
+                </div>
+            </div>
+    
+        </div>
+    </div>
+    
+    <div class="bottom_footer">
 
     </div>
 </footer>
