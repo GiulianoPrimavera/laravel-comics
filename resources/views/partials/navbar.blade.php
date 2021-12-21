425f4a6254fb8d4aa3373c1188814aacb5b901b3
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-21 12:12:05 +0100

    adds navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,20 @@
+<div class="my_navbar row">
+    <div class="logo_container col">
+        <img src="" alt="">
+    </div>
+    <div class="navbar_links_container col container">
+        <ul class="row">
+            <li class="col"><a href="">CHARACTERS</a></li>
+            <li class="col"><a href="">COMICS</a></li>
+            <li class="col"><a href="">MOVIES</a></li>
+            <li class="col"><a href="">TV</a></li>
+            <li class="col"><a href="">GAMES</a></li>
+            <li class="col"><a href="">COLLECTIBLES</a></li>
+            <li class="col"><a href="">VIDEOS</a></li>
+            <li class="col"><a href="">FANS</a></li>
+            <li class="col"><a href="">NEWS</a></li>
+            <li class="col">SHOPS</li>
+        </ul>
+        <div>search bar</div>
+    </div>
+</div>
