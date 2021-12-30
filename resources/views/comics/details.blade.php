e8dc23ad0efe46705046d9775a14b12735c2e9ba
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-30 18:43:17 +0100

    adds main structure for the details page

--- resources/views/comics/details.blade.php
+++ resources/views/comics/details.blade.php
@@ -5,5 +5,50 @@
 @endsection
 
 @section('content')
-    <h1>titolo dei dettagli {{ $comic["title"] }}</h1>
+    <main>
+
+        <div class="thumb_holder">
+            <img src="{{ $comic["thumb"] }}" alt="">
+        </div>
+        
+        
+        <div class="comic_main_container">
+            
+            <div class="comic_description">
+    
+                <h2>{{-- titolo fumettto --}}</h2>
+
+                <div class="price_info">
+
+                </div>
+
+                <p>{{-- descrizione del fumetto --}}</p>
+
+            </div>
+
+            <div class="advertisement">
+                <img src="" alt="">
+            </div>
+        </div>
+
+
+        <div class="comic_info_container">
+
+            <div class="talent">
+                <div class="art_by"></div>
+
+                <div class="written_by"></div>
+            </div>
+
+            <div class="specs">
+                <div class="series"></div>
+
+                <div class="price"></div>
+
+                <div class="on_sale_date"></div>
+            </div>
+        </div>
+
+
+    </main>
 @endsection
