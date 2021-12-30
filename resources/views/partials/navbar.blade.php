153b00dc8452b36950458a4c937af46c69d3f3c2
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-30 16:12:09 +0100

    adds array for the navbar links, adds class active based on the route name

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,3 +1,57 @@
+@php
+
+$navLinks = [
+    [
+        "linkName" => "CHARACTERS",
+        "linkNameRoute" => ""
+    ],
+    [
+        "linkName" => "COMICS",
+        "linkNameRoute" => "comics"
+    ],
+    [
+        "linkName" => "MOVIES",
+        "linkNameRoute" => ""
+    ],
+    [
+        "linkName" => "TV",
+        "linkNameRoute" => ""
+    ],
+    [
+        "linkName" => "GAMES",
+        "linkNameRoute" => ""
+    ],
+    [
+        "linkName" => "COLLECTIBLES",
+        "linkNameRoute" => ""
+    ],
+    [
+        "linkName" => "VIDEOS",
+        "linkNameRoute" => ""
+    ],
+    [
+        "linkName" => "FANS",
+        "linkNameRoute" => ""
+    ],
+    [
+        "linkName" => "NEWS",
+        "linkNameRoute" => ""
+    ],
+    [
+        "linkName" => "SHOPS",
+        "linkNameRoute" => ""
+        ]
+];
+
+    /*
+        la parola Request indica una classe di laravel, in essa sono contenute tutta una serie di informazioni 
+        che gestiscono la comunicazione tra il client e il server.
+        una di queste è il nome della route della pagina attualmente visualizzata, 
+        a questa informazione si accede scrivendo la seguente riga
+    */
+    $routeName = Request::route()->getName();
+@endphp
+
 <header>
     <div class="container">
         <div class="my_navbar row">
@@ -6,17 +60,19 @@
             </div>
             <div class="navbar_links_container col container">
                 <ul class="row align-items-center">
-                    <li class="col"><a href="">CHARACTERS</a></li>
-                    <li class="col"><a href="">COMICS</a></li>
-                    <li class="col"><a href="">MOVIES</a></li>
-                    <li class="col"><a href="">TV</a></li>
-                    <li class="col"><a href="">GAMES</a></li>
-                    <li class="col"><a href="">COLLECTIBLES</a></li>
-                    <li class="col"><a href="">VIDEOS</a></li>
-                    <li class="col"><a href="">FANS</a></li>
-                    <li class="col"><a href="">NEWS</a></li>
-                    <li class="col"><a href="">SHOPS</a></li>
+                    @foreach ($navLinks as $singleLink)
+
+                    {{-- stampo ogni singolo elemento dall'array $navLinks --}}
+                    {{-- ogni elemento nell'array ha due voci: il titolo del link, il nome della route del link --}}
+                    {{-- se il nome della rpute del link è uguale al nome della route in cui si trova la pagina attualmente visualizzata a schermo --}}
+                    {{-- allora gli aggiungo la classe active, altrimenti no --}}
+                        <li class="col"><a href="#" class="{{ $routeName === $singleLink["linkNameRoute"] ? 'active' : '' }}">{{ $singleLink["linkName"] }}</a></li>
+                        
+                    @endforeach
+
+                    {{-- questo dovrebbe essere un input di testo --}}
                     <li class="col">Search</li>
+
                 </ul>
             </div>
         </div>
