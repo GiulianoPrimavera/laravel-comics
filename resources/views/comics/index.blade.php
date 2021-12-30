0b9c452ce27eb024dddcc1569bb5421397056bb7
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-30 16:49:00 +0100

    rename folder

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.layout_base')
+
+@section('title')
+    comics
+@endsection
+
+{{-- qui si trova l'header con la navbar --}}
+
+@section('content')
+    {{-- qui si trova il main --}}
+    <main>
+        <div class="container">
+            
+            <div class="badge">
+                <h2>
+                    CURRENT SERIES
+                </h2>
+            </div>
+
+            @foreach ($comics as $item)
+                <div class="single_card_container">
+                    <a href="{{ route('comics.details') }}">
+                        <div class="comic_thumb_container">
+                            <img src="{{$item['thumb']}}" alt="">
+                        </div>
+                        <div class="comic_title">
+                            @php
+
+                                /* if(str_contains($item["title"], "#")){
+                                    $actualTitle = explode('#', $item['title'])
+                                }else if(str_contains($item["title"], "Vol"){
+                                    $actualTitle = explode('Vol', $item['title'])
+                                } */
+                                $actualTitle = explode('#', $item['title'])
+
+                            @endphp
+                                
+                            <p>{{ strtoupper($actualTitle[0]) }}</p>
+                        </div>
+                    </a>
+                </div>
+            @endforeach
+
+        </div>
+    </main>
+@endsection
+
+{{-- qui si trova il footer --}}
