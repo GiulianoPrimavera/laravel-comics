4a3dfaddea6018672fde0aee37fa6a5d92683db2
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-30 19:41:12 +0100

    adds elements for the comic_main_container

--- resources/views/comics/details.blade.php
+++ resources/views/comics/details.blade.php
@@ -18,18 +18,39 @@
             
             <div class="comic_description">
                 
-                <h2>{{ strtoupper($comic["title"]) }}</h2>
+                <h3>{{ strtoupper($comic["title"]) }}</h3>
 
                 <div class="price_info">
+                    <div class="price">
+                        
+                        <p>U.S. Price <span>{{ $comic["price"] }}</span></p>
 
+                    </div>
+                    <div class="availability">
+                        <p class="availability_status">
+                            AVAILABLE
+                        </p>
+                        <div class="dropdown">
+                            <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span> Dropdown button</span>
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                              <a class="dropdown-item" href="#">Action</a>
+                              <a class="dropdown-item" href="#">Another action</a>
+                              <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </div>
+                        
+                    </div>
                 </div>
 
-                <p>{{-- descrizione del fumetto --}}</p>
+                <p class="description">{{$comic["description"]}}</p>
 
             </div>
 
             <div class="advertisement">
-                <img src="" alt="">
+                <h6>ADVERTISEMENT</h6>
+                <img src="{{ asset('img/adv.jpg') }}" alt="">
             </div>
         </div>
 
