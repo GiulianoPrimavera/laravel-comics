d127f01558f75cdb784ea5f5671bee3fcf94b908
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2021-12-30 18:30:20 +0100

    adds route for every single comic

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -19,18 +19,23 @@
 
             @foreach ($comics as $item)
                 <div class="single_card_container">
-                    <a href="{{ route('comics.details') }}">
+                    {{-- 
+                    alla funzione route, che identifica la rotta della pagina, 
+                    passiamo un'altro elemento. In questo caso il secondo elemento è un 
+                    array associativo che ha lo stesso nome del secondo parametro passato nella rotta della 
+                    view (in questo caso "id").
+                    Laravel automaticamente comporrà la rotta mettendo insieme questi due parametri
+                    Utilizzando $loop->index (una funzione all'interno di Laravel) possiamo risalire all'indice
+                    dell'elemento cliccato
+                    --}}
+                    
+                    <a href="{{ route('comics.details', ["id" => $loop->index]) }}">
                         <div class="comic_thumb_container">
                             <img src="{{$item['thumb']}}" alt="">
                         </div>
                         <div class="comic_title">
                             @php
 
-                                /* if(str_contains($item["title"], "#")){
-                                    $actualTitle = explode('#', $item['title'])
-                                }else if(str_contains($item["title"], "Vol"){
-                                    $actualTitle = explode('Vol', $item['title'])
-                                } */
                                 $actualTitle = explode('#', $item['title'])
 
                             @endphp
